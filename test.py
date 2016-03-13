78ab74dab6331ec988769a3b561d1486685e6812
Tyler Wright <tyler.wright@hds.com>
2016-03-12 23:23:50 -0500

    Finished adding unit tests for get_cpu_information

--- test.py
+++ test.py
@@ -30,5 +30,65 @@ class CpuTest(unittest.TestCase):
         # Check to make sure the returned value is an int
         self.assertEqual(type(cpu_physical_cores), int)
         
+    def test_cpu_logical_cores_value(self):
+        """
+        Tests the get_cpu_information function for cpu_logical_cores
+        """
+        
+        cpu_architecture, cpu_model, cpu_physical_cores, cpu_logical_cores = get_cpu_information()
+        
+        # Check to make sure the number of logical cores is 8
+        self.assertEqual(cpu_logical_cores, 8)
+    
+    def test_cpu_logical_cores_type(self):
+        """
+        Tests the get_cpu_information function for cpu_logical_cores
+        """
+        
+        cpu_architecture, cpu_model, cpu_physical_cores, cpu_logical_cores = get_cpu_information()
+        
+        # Check to make sure the returned value is an int
+        self.assertEqual(type(cpu_logical_cores), int)
+        
+    def test_cpu_architecture_value(self):
+        """
+        Tests the get_cpu_information function for cpu_architecture
+        """
+        
+        cpu_architecture, cpu_model, cpu_physical_cores, cpu_logical_cores = get_cpu_information()
+        
+        # Check to make sure the returned value is "x86_64"
+        self.assertEqual(cpu_architecture, 'x86_64')
+        
+    def test_cpu_architecture_type(self):
+        """
+        Tests the get_cpu_information function for cpu_architecture
+        """
+        
+        cpu_architecture, cpu_model, cpu_physical_cores, cpu_logical_cores = get_cpu_information()
+        
+        # Check to make sure the returned value is a string
+        self.assertEqual(type(cpu_architecture), str)
+        
+    def test_cpu_model_value(self):
+        """
+        Tests the get_cpu_information function for cpu_model
+        """
+        
+        cpu_architecture, cpu_model, cpu_physical_cores, cpu_logical_cores = get_cpu_information()
+        
+        # Check to make sure the returned value is "Intel(R) Core(TM) i7-4771 CPU @ 3.50GHz"
+        self.assertEqual(cpu_model, 'Intel(R) Core(TM) i7-4771 CPU @ 3.50GHz')
+        
+    def test_cpu_model_type(self):
+        """
+        Tests the get_cpu_information function for cpu_model
+        """
+        
+        cpu_architecture, cpu_model, cpu_physical_cores, cpu_logical_cores = get_cpu_information()
+        
+        # Check to make sure the returned value is a string
+        self.assertEqual(type(cpu_model), str)
+        
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
